20b7ceb67e85dda2f7cde4f403b85316ba11ef94
Lirmall <andreyk-orlangyr@mail.ru>
2021-11-06 17:12:29 +0300

    Добавление мультипарт-файла

--- src/main/java/ru/klokov/restguitarproject/controller/admin/AccordController.java
+++ src/main/java/ru/klokov/restguitarproject/controller/admin/AccordController.java
@@ -1,13 +1,15 @@
 package ru.klokov.restguitarproject.controller.admin;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import javax.validation.constraints.NotBlank;
 
 @RestController
 @RequestMapping("/admin/accord")
 public class AccordController {
 
-    public void add() {
+    @PostMapping(consumes = {"multipart/form-data"})
+    public void add(@RequestPart("name") @NotBlank String name, @RequestPart("fingerPosition") String fingerPosition, @RequestPart("image") MultipartFile image) {
 
     }
 
