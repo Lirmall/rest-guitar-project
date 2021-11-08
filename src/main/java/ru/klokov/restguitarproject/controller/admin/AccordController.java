8327074ab86cd9eab74c4e2ef738fedc4258f7da
Lirmall <andreyk-orlangyr@mail.ru>
2021-11-08 12:05:23 +0300

    Добавлен инструмент для добавления аккордов и изображений аккордов в папку проекта

--- src/main/java/ru/klokov/restguitarproject/controller/admin/AccordController.java
+++ src/main/java/ru/klokov/restguitarproject/controller/admin/AccordController.java
@@ -1,15 +1,32 @@
 package ru.klokov.restguitarproject.controller.admin;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
 import javax.validation.constraints.NotBlank;
+import java.io.*;
+
 
 @RestController
 @RequestMapping("/admin/accord")
 public class AccordController {
 
-    @PostMapping(consumes = {"multipart/form-data"})
-    public void add(@RequestPart("name") @NotBlank String name, @RequestPart("fingerPosition") String fingerPosition, @RequestPart("image") MultipartFile image) {
+    @PostMapping(value = "/upload", consumes = {"multipart/form-data"})
+    public void add(@RequestPart("name") @NotBlank String name,
+                    @RequestPart("fingerPosition") @NotBlank String fingerPosition,
+                    @RequestPart("image") @NotBlank MultipartFile image) {
+
+        try(BufferedOutputStream bufferedStream = new BufferedOutputStream(
+                new FileOutputStream(
+                        "J:\\Develop\\rest-guitar-project\\src\\main\\resources\\static\\images\\accords\\" + name + ".jpg"))) {
+            byte [] imageBytes = image.getBytes();
+            bufferedStream.write(imageBytes);
+        } catch (Exception e) {
+            System.out.println("File not upload " + e);
+        }
 
     }
 
