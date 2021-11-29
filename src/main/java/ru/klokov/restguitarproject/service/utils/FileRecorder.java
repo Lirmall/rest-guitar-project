2aa889c3cdd70c8cd3b32405762bab7ed4149e6f
Lirmall <andreyk-orlangyr@mail.ru>
2021-11-29 14:10:15 +0300

    Изменена структура и методы интерфейса FileRecorder

--- src/main/java/ru/klokov/restguitarproject/service/utils/FileRecorder.java
+++ src/main/java/ru/klokov/restguitarproject/service/utils/FileRecorder.java
@@ -2,8 +2,13 @@ package ru.klokov.restguitarproject.service.utils;
 
 import ru.klokov.restguitarproject.model.dto.AdminDTO;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public interface FileRecorder {
 
-    void add(AdminDTO adminDTO);
+    void add(AdminDTO adminDTO) throws IOException;
+
+    void write(AdminDTO adminDTO, String filePath) throws IOException;
 
 }
