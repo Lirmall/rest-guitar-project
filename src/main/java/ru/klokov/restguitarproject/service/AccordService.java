b2f32f544e6482c845e392a038dfb667dc7debad
Lirmall <andreyk-orlangyr@mail.ru>
2021-12-09 15:42:16 +0300

    Добавлена отправка запроса на работу с базой данных

--- src/main/java/ru/klokov/restguitarproject/service/AccordService.java
+++ src/main/java/ru/klokov/restguitarproject/service/AccordService.java
@@ -2,6 +2,7 @@ package ru.klokov.restguitarproject.service;
 
 import org.springframework.stereotype.Service;
 import ru.klokov.restguitarproject.model.dto.AccordDTO;
+import ru.klokov.restguitarproject.persistence.admin.AdminAccordRepository;
 import ru.klokov.restguitarproject.service.utils.FileRecorder;
 
 import java.io.IOException;
@@ -10,12 +11,15 @@ import java.io.IOException;
 public class AccordService {
 
     FileRecorder imageFileRecorder;
+    AdminAccordRepository adminAccordRepository;
 
-    public AccordService(FileRecorder imageFileRecorder) {
+    public AccordService(FileRecorder imageFileRecorder, AdminAccordRepository adminAccordRepository) {
         this.imageFileRecorder = imageFileRecorder;
+        this.adminAccordRepository = adminAccordRepository;
     }
 
     public void add(AccordDTO accordDTO) throws IOException {
         imageFileRecorder.add(accordDTO);
+        adminAccordRepository.add(accordDTO);
     }
 }
